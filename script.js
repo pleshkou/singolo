f51f40d11fe1977facfb2bea7a498480950516f7
Alex Pleshkou <pla@tut.by>
2020-03-21 14:47:02 +0300

    feat: Singolo DOM-2))

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const TAB = document.getElementById('tab');
 const WRAPPER = document.getElementById('wrapper');
 const FORM = document.forms.informat;
 const MODAL = document.querySelector('.modal')
+const anchor = document.querySelectorAll('#menu>li>a')
 
-MENU.addEventListener('click', (event) => {
+
+const anchor_home = document.querySelector('a[name=home]').offsetTop;
+const anchor_services = document.querySelector('a[name=services').offsetTop;
+const anchor_portfolio = document.querySelector('a[name=portfolio]').offsetTop;
+const anchor_about = document.querySelector('a[name=about]').offsetTop;
+const anchor_contacts = document.querySelector('a[name=contacts]').offsetTop;
+
+
+document.addEventListener('scroll', onScroll);
+
+
+function onScroll(event) {
+    let cur_pos = window.scrollY;
+    let cur_anchor = 
+        cur_pos >= anchor_contacts ? document.querySelector('a[href$=contacts]') :
+        cur_pos >= anchor_about ? document.querySelector('a[href$=about]') :
+        cur_pos >= anchor_portfolio ? document.querySelector('a[href$=portfolio]') :
+        cur_pos >= anchor_services ? document.querySelector('a[href$=services') :
+        document.querySelector('a[href$=home]');
     MENU.querySelectorAll('a').forEach(el => {el.classList.remove('active')});
-    event.target.classList.add('active');
-})
+    cur_anchor.classList.add('active');
+}
+
+
 
 PICTURES.addEventListener('click', event => {
     PICTURES.querySelectorAll('img').forEach(el => {el.classList.remove('active')});
@@ -81,9 +102,11 @@ function showData(f) {
     }
     
     MODAL.querySelector('.inner').innerText = out;
-    
+    f.reset();
+
 }
 
 MODAL.querySelector('.butt').addEventListener('click', e => {
     MODAL.style.display = 'none';
 })
+
